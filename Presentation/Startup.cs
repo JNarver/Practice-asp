5e45ae36ad5a3958c93bb91b1cf7ba4297428845
NarverAlison <narver0399@gmail.com>
2020-06-10 16:57:10 +0300

    First start, reworked setup, bundled scripts, adapted styles.

--- Presentation/Startup.cs
+++ Presentation/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,23 +16,26 @@ namespace Presentation
         public void ConfigureServices(IServiceCollection services)
         {
             //Добовляем поддержку контроллеров и представлений (MVC)
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            // порядок регистрации midleware(функционала) имеет значение
+
+            if (env.IsDevelopment())// что б в процессе разработки получать информацию об ошибках
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
+            //поддержка статических файлов
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //регистрация нужных маршрутов(endpoints)
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");//по деф. хоум контроллер, действие индекс home.com
             });
         }
     }
